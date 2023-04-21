e17e2137c49b38092c44ac51cad89ce7aa923918
taimur <101434467+voidbydefault@users.noreply.github.com>
2023-04-21 19:32:23 +0300

    Update main.py
    Replaced deprecated functionality for Chrome and Selenium

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from linkedineasyapply import LinkedinEasyApply
 from validate_email import validate_email
 
@@ -13,7 +14,8 @@ def init_browser():
     for option in options:
         browser_options.add_argument(option)
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=browser_options)
 
     driver.set_window_position(0, 0)
     driver.maximize_window()
