0a38f4571bd16f72fa4eda431db6f844ce94ae71
Ayoub Berrag <berrag.ayoub@gmail.com>
2023-07-03 22:33:17 -0400

    Update main.py
    Fix Issue (#25)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from linkedineasyapply import LinkedinEasyApply
 from validate_email import validate_email
+from selenium.webdriver.chrome.service import Service
 
 def init_browser():
     browser_options = Options()
@@ -12,8 +13,8 @@ def init_browser():
 
     for option in options:
         browser_options.add_argument(option)
-
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=browser_options,)
 
     driver.set_window_position(0, 0)
     driver.maximize_window()
